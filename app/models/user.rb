a8a2eaa2dd7d5beb65196ffc523fc545150712e0
Mesam Timmar <mesam.timmar@square63.com>
2016-06-28 18:03:03 +0500

    User: Displayed profile picture at home page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+  has_one :attachment, as: :attachable
+  accepts_nested_attributes_for :attachment
+
+  def show_profile_picture(style = :original)
+    profile_picture = self.attachment
+    profile_picture ? profile_picture.try(:image).url(style) : "#{style.to_s}/missing.png"
+  end
 end
