87f611c6619b57a24d215abb00a4f760ce1adfd3
Mesam Timmar <mesam.timmar@square63.com>
2016-06-28 18:01:19 +0500

    Devise: Added upload profile picture functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,16 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_sign_up_params, if: :devise_controller?
   before_action :configure_account_update_params, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [attachment_attributes: [:id, :image, :_destroy]])
+  end
 
   def configure_sign_up_params
-    devise_parameter_sanitizer.for(:sign_up) << :full_name
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name, :email, :password, :password_confirmation, attachment_attributes: [:id, :image, :_destroy]])
   end
 
   def configure_account_update_params
