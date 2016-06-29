2fad7d1f090bb0c0f5d5da2c07580c08a0ae1b88
Mesam Timmar <mesam.timmar@square63.com>
2016-06-29 12:43:00 +0500

    Actor: Generated migration for actor

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160628131224) do
+ActiveRecord::Schema.define(version: 20160629073015) do
+
+  create_table "actors", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.string   "gender",     limit: 255
+    t.text     "biography",  limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
 
   create_table "attachments", force: :cascade do |t|
     t.string   "image_file_name",    limit: 255
