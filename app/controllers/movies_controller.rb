8a72eb66187e0bdde06de65156a26a71c26a0b70
Mesam Timmar <mesam.timmar@square63.com>
2016-06-29 12:04:16 +0500

    Movie: Used nested form to add posters to movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -69,6 +69,6 @@ class MoviesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def movie_params
-      params.require(:movie).permit(:title, :trailer, :description, :approved, :featured)
+      params.require(:movie).permit(:title, :trailer, :description, :approved, :featured, posters_attributes: [:id, :image, :_destroy])
     end
 end
