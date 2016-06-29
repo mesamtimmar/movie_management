076e27f4c22151d2d1f27ccef3dadf340a16cb0a
Mesam Timmar <mesam.timmar@square63.com>
2016-06-29 11:58:52 +0500

    Movie: Added association with attachment

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,7 @@
 class Movie < ActiveRecord::Base
+  has_many :posters, class_name: "Attachment", as: :attachable, dependent: :destroy
+  accepts_nested_attributes_for :posters, allow_destroy: true
+
   def show_desc
     self.description.to_s.html_safe
   end
