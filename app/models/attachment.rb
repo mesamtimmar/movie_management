76dab2c296f3812d6a48136412a3a0a75d7c08ef
Mesam Timmar <mesam.timmar@square63.com>
2016-06-29 16:24:22 +0500

    Attachment: Did some indentation

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,7 +1,12 @@
 class Attachment < ActiveRecord::Base
-  has_attached_file :image, 
-  styles: { medium: "300x300>", thumb: "100x100>" }, 
-  default_url: ":style/missing.png"
+  has_attached_file :image,
+                    styles: {
+                              medium: "300x300>",
+                              thumb: "100x100>"
+                            },
+                    default_url: ":style/missing.png"
+
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
   belongs_to :attachable, polymorphic: true
 end
