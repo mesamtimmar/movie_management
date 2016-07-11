fb7d9555d026dd4d3cb991021b305e97e923781b
Mesam Timmar <mesam.timmar@square63.com>
2016-07-11 14:52:02 +0500

    Actors: Added actor entries through seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+actor_list = [
+  ["Al Pacino", "male", "Best Actor of All time"],
+  ["Robert di Noire", "male", "Best Actor of All time"],
+  ["Marlon Brando", "male", "Best Actor of All time"],
+  ["Denzal Washington", "male", "Best Actor of All time"]
+]
+
+actor_list.each do |name, gender, biography|
+  Actor.create(name: name, gender: gender, biography: biography)
+end
