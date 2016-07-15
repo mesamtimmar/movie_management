4790a85e99c426c2ceca9ca70255758ddcc968fd
Mesam Timmar <mesam.timmar@square63.com>
2016-07-15 19:16:33 +0500

    Moive: Displayed posters on edit page

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -2,4 +2,8 @@ module MoviesHelper
   def formated_date(date)
     date.strftime('%d/%m/%Y') if date.present?
   end
+
+  def poster_present?(poster)
+    poster.object.image.url(:thumb) == 'thumb/missing.png'
+  end
 end
