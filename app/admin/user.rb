11f8971e47361e2d7aea03fd445e6d05a1f570b9
Mesam Timmar <mesam.timmar@square63.com>
2016-07-18 15:41:06 +0500

    ActiveAdmin: Added user to admin panel

---
+++ app/admin/user.rb
@@ -0,0 +1,20 @@
+ActiveAdmin.register User do
+  permit_params :email, :password
+
+  index do
+    column :email
+    column 'Image' do |user|
+      image_tag(user.attachment.image.url(:thumb)) if user.attachment
+    end
+    column :created_at
+    actions
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :email
+      f.input :password
+    end
+    f.actions
+  end
+end
