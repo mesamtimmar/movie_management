81e87943a521251d5482ea5022b44292dfbb5f36
Mesam Timmar <mesam.timmar@square63.com>
2016-07-25 17:57:27 +0500

    Home: Displayed approved movies only

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
 
   def index
-    @latest = Movie.latest_movies.first(4)
-    @featured = Movie.featured_movies.first(4)
-    @top_rated_movies = Movie.top_rated.first(4)
+    @latest = Movie.latest_movies.approved.first(4)
+    @featured = Movie.featured_movies.approved.first(4)
+    @top_rated_movies = Movie.top_rated.approved.first(4)
   end
 end
