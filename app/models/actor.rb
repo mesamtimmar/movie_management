3697296eb591304c5f026066adaac209e856a0ac
Mesam Timmar <mesam.timmar@square63.com>
2016-07-28 18:29:07 +0500

    Actor: Added depenent destroy

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,6 +1,6 @@
 class Actor < ActiveRecord::Base
   has_many :casts
-  has_many :movies, through: :casts
+  has_many :movies, through: :casts, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :gender, presence: true, length: { maximum: 10 }
