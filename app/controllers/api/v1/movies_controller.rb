991b2e2ba90ad7bb2f3998db24c4a522a4ed25f3
Mesam Timmar <mesam.timmar@square63.com>
2016-07-28 16:38:14 +0500

    Movie: Added authentication for API access

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -2,13 +2,10 @@ module Api
   module V1
     class MoviesController < ApplicationController
       respond_to :json
+      before_action :authenticate_request
 
       def index
-        if (params[:title] || params[:genre] || params[:actors] || params[:release_date])
-          respond_with Movie.search_movie(params)
-        else
-          respond_with Movie.all
-       end
+        respond_with Movie.search_movies(params).page(params[:page])
       end
 
       def show
@@ -16,8 +13,15 @@ module Api
         if @movie.present?
           respond_with @movie.details_hash
         else
-          respond_with []
+          respond_with ["Error": "No Record Found"]
         end
+      end
+
+      def authenticate_request
+       if request && request.headers && request.headers['Authorization']
+         return if request.headers['Authorization'] == User::TOKEN
+       end
+       head :unauthorized
       end
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  TOKEN = 'ad2323wmkljl2kj2lj3l2l3k'
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   has_one :attachment, as: :attachable
